5f1c665955d771be076e83bef174b9a9df227c63
Matt Campbell <matt@mattcampbell.net>
2011-12-20 20:21:33 -0500

    Adding minifier

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
 java -cp .:lib/js.jar org.mozilla.javascript.tools.shell.Main lib/compile.js game.coffee
+
+if [ $? -ne 0 ] ; then
+	echo "CoffeeScript compilation failed"
+	exit
+fi
+
+java -jar lib/compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js game.js --js_output_file game.min.js
