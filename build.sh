7ffe110f6a25f60fb49474bb6d3547fd1f02579c
Matt Campbell <matt@mattcampbell.net>
2011-12-20 21:50:00 -0500

    Changing paths around
    --HG--
    rename : game.coffee => src/game.coffee

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-java -cp .:lib/js.jar org.mozilla.javascript.tools.shell.Main lib/compile.js game.coffee
+java -cp .:lib/js.jar org.mozilla.javascript.tools.shell.Main lib/compile.js src/game.coffee
 
 if [ $? -ne 0 ] ; then
 	echo "CoffeeScript compilation failed"
 	exit
 fi
 
-java -jar lib/compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js game.js --js_output_file game.min.js
+java -jar lib/compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js bin/game.js --js_output_file bin/game.min.js
