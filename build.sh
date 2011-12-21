6f787c68aee5eac679a3264ae745852230099134
Matt Campbell <matt@mattcampbell.net>
2011-12-20 22:45:16 -0500

    Setting up file concatenation for CoffeeScript compilation
    --HG--
    rename : src/game.coffee => src/main.coffee

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
-java -cp .:lib/js.jar org.mozilla.javascript.tools.shell.Main lib/compile.js src/game.coffee
+echo "Compiling..."
+
+find src/ -name '*.coffee' \
+| grep -Ev '^src/main.coffee$' \
+| xargs cat \
+| cat - src/main.coffee \
+| java -cp .:lib/js.jar org.mozilla.javascript.tools.shell.Main lib/compile.js
 
 if [ $? -ne 0 ] ; then
 	echo "CoffeeScript compilation failed"
 	exit
 fi
+
+echo "Minifying..."
 
 java -jar lib/compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js bin/game.js --js_output_file bin/game.min.js
